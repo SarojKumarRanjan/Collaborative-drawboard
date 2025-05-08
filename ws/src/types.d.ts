ee9b34e3dce0be1256acdb991fc1f542bad5c706
SarojKumarRanjan <sarojranjan555@gmail.com>
2025-05-08 10:35:50 +0530

    ws server setup

---
+++ ws/src/types.d.ts
@@ -0,0 +1,18 @@
+
+declare global{
+    interface CtxOptions{
+        lineWidth:number;
+        lineColor:string;
+    }
+
+   interface serverToClientEvents{
+        socket_draw:(newMoves:[number,number][],options:CtxOptions)=>void;
+        
+    }
+ interface clientToServerEvents{
+        draw:(moves:[number,number][],options:CtxOptions)=>void;
+        
+    }
+}
+
+export {}
